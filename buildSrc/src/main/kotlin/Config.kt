643cdd4e3fb772ae1b18aca6c9dc101f983c4eff
v1tzor <dev.aleshin@gmail.com>
2024-11-29 22:58:31 +0300

    feat: release new version

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -24,8 +24,8 @@ object Config {
     const val targetSdkVersion = 34
     const val minSdkVersion = 24
 
-    const val versionCode = 30
-    const val versionName = "1.0.0"
+    const val versionCode = 31
+    const val versionName = "1.0.1"
 
     const val testInstrumentRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val consumerProguardFiles = "consumer-rules.pro"
