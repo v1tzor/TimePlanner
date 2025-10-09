450aaf7ff9ec5ce6fdff9ca13dbf645bd01572ac
M <treeentertainment@outlook.kr>
2025-10-09 10:18:03 +0900

    Update Common.kt

--- core/utils/src/main/java/ru/aleshin/core/utils/extensions/Common.kt
+++ core/utils/src/main/java/ru/aleshin/core/utils/extensions/Common.kt
@@ -32,5 +32,6 @@ fun Int?.toStringOrEmpty() = this?.toString() ?: ""
 fun generateUniqueKey() = UUID.randomUUID().mostSignificantBits and Long.MAX_VALUE
 
 fun String.substringOrFull(startIndex: Int, endIndex: Int): String {
-    return if (lastIndex >= endIndex) substring(startIndex, endIndex) else this
+    return if (length >= endIndex) substring(startIndex, endIndex) else this
 }
+
