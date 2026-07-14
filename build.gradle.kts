9eb77cbcf7bddd282b3c733866328435895bf7d4
Stanislav Aleshin <dev.aleshin@gmail.com>
2026-07-14 13:15:08 +0300

    feat: updated libs versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.compose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.parcelize) apply false
